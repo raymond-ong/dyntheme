b374101a955898a507df03bf5fe0a8748d464907
raymond-ong <remondo@gmail.com>
2021-03-31 18:22:14 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+const themes = [
+  {name: "dark", label: "Dark", bgColor: "black", txtColor: "white"},
+  {name: "light", label: "Light", bgColor: "lightgray", txtColor: "black"},
+  {name: "colorful", label: "Colorful", bgColor: "lightblue", txtColor: "darkred"},
+]
 
 function App() {
+  const [themeSelected, setThemeSelected] = useState({theme: 'dark'})
+
+  const handleThemeChange = (e) => {
+    console.log("[handleThemeChange] Selected Theme:", e.target.value)
+    setThemeSelected({
+      theme: e.target.value
+    })
+  }
+
+  const getStyle = () => {
+    if (!themeSelected) {
+      return null;
+    }
+    const themeObj = themes.find(x => x.name === themeSelected.theme);
+    if (!themeObj) {
+      return null;
+    }
+
+    const styleText = `:root {--main-bg-color: ${themeObj.bgColor};--main-text-color: ${themeObj.txtColor};}`;
+    console.log(styleText);
+    return  <style type="text/css">{styleText}</style>;
+  }
+
+  console.log("[App] Displaying theme", themeSelected);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {getStyle()}
+      <div className="Header">
+      <label htmlFor="cbxThemes" className="formLabel">Choose a theme:</label>
+      <select name="cbxThemes" id="cbxThemes" 
+        onChange={ handleThemeChange }
+      >
+        {themes.map(theme => <option key={theme.name} value={theme.name}>{theme.label}</option>)}
+      </select>
+      </div>
+
+      <div className="Body">
+        <h1>Body Part</h1>
+        <h2>Some Content...</h2>
+      </div>
     </div>
   );
 }
